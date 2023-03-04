5bbe32c5edd05c05c9ffbc31b2979c21d942da2e
Naweed Meer <naweed.meer@sigmatechnology.se>
2023-03-04 14:38:39 +0100

    Added ball script and model, plus walls.

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballscript : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+
+    Vector3 forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void MoveFunction()
+    {
+        forward = transform.right;
+
+        if (Input.GetKey(Forward))
+        {
+            rb.AddForce(movementSpeed * forward);
+        }
+        if (Input.GetKey(Backward))
+        {
+            rb.AddForce(-movementSpeed * forward);
+        }
+        if (Input.GetKey(Right))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotationalSpeed);
+        }
+        if (Input.GetKey(Left))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * rotationalSpeed);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        Transform transWheel1 = transform.GetChild(0);
+        Transform transWheel2 = transform.GetChild(1);
+        Transform transWheel3 = transform.GetChild(2);
+        Transform transWheel4 = transform.GetChild(3);
+
+        return (Physics.Raycast(transWheel1.position, Vector3.down, groundBound) ||
+                Physics.Raycast(transWheel2.position, Vector3.down, groundBound) ||
+                Physics.Raycast(transWheel3.position, Vector3.down, groundBound) ||
+                Physics.Raycast(transWheel4.position, Vector3.down, groundBound));
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(Up))
+        {
+            rb.AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
